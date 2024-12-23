0d7af045c904d5018422d6b0d5983f50231730dc
ktunebe <ktunebe@gmail.com>
2024-12-23 09:55:19 -0600

    initial commit

---
+++ src/test/java/dev/kent/recipe_book/RecipeBookApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kent.recipe_book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecipeBookApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
