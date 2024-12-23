dcfa03e932f4098d3d3b99e85e9ffe07c866b3af
ktunebe <ktunebe@gmail.com>
2024-12-23 10:34:21 -0600

    configured mongodb server

--- src/main/java/dev/kent/recipe_book/RecipeBookApplication.java
+++ src/main/java/dev/kent/recipe_book/RecipeBookApplication.java
@@ -2,6 +2,7 @@ package dev.kent.recipe_book;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class RecipeBookApplication {
